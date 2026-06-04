a5519244b7f5ddb451a29cfb632f1593394bed22
S-havl <pansillomcvalentincampostorres@gmail.com>
2026-06-03 23:50:44 -0500

    feat(vbe): getting information from VBE

--- arch/x86_64/boot/stage2.asm
+++ arch/x86_64/boot/stage2.asm
@@ -35,18 +35,30 @@ stage2_start:
 ; SET VBE MODE 1024x768x32 
 ; ==========================================================
 
-	mov ax, 0x4F01
-	mov cx, 0x0118
-	mov di, vbe_mode_info_block
+	mov	ax, 0x4F01
+	mov	cx, 0x0118
+	mov	di, vbe_mode_info_block
 	
-	push ds
-	pop es
+	push	ds
+	pop	es
 	
-	int 0x10
+	int	0x10
 
-	mov ax, 0x4F02
-	mov bx, 0x4118
-	int 0x10
+	mov	ax, 0x4F02
+	mov	bx, 0x4118
+	int	0x10
+
+	mov	eax, [vbe_mode_info_block + 0x28]
+	mov	[framebuffer_phys], eax
+
+	mov	ax, [vbe_mode_info_block + 0x12]
+	mov	[screen_width], ax
+
+	mov	ax, [vbe_mode_info_block + 0x14]
+	mov	[screen_height], ax
+
+	mov	ax, [vbe_mode_info_block + 0x10]
+	mov	[screen_pitch], ax
 
 	jmp	enter_protect_mode	; Transition to PM
 
@@ -88,6 +100,10 @@ DATA_SEL	equ	2 << 3
 
 boot_drive:	db	0
 vbe_mode_info_block:	times	256	db	0
+framebuffer_phys:	dd	0
+screen_width:	dw	0
+screen_height:	dw	0
+screen_pitch:	dw	0
 
 ; ==================================================
 ;                 PROTECTED MODE
