abc7f5cd163d88e491a9ed053db02098a757d5d7
S-havl <pansillomcvalentincampostorres@gmail.com>
2026-06-07 10:29:09 -0500

    feat(isr): implement x86_64 common ISR handler and register exports

--- arch/x86_64/interrupts/isr_stubs.asm
+++ arch/x86_64/interrupts/isr_stubs.asm
@@ -1,8 +1,55 @@
-section .text
+section	.text
+
 global	isr0
+global	isr_common
 
 isr0:
 	mov	byte [0xB8000], 'X'
 	mov	byte [0xB8001], 0x4F
 
 isr_common:
+	push	r15
+	push	r14
+	push	r13
+	push	r12
+	push	r11
+	push	r10
+	push	r9
+	push	r8
+
+	push	rdi
+	push	rsi
+	push	rbp
+	push	rdx
+	push	rcx
+	push	rbx
+	push	rax
+
+	mov	rax, rsp
+	and	rsp, -16
+
+	mov	rdi, rsp
+;	call	interrupt
+
+	mov	rsp, rax
+
+	pop	rax
+	pop	rbx
+	pop	rcx
+	pop	rdx
+	pop	rbp
+	pop	rsi
+	pop	rdi
+
+	pop	r8
+	pop	r9
+	pop	r10
+	pop	r11
+	pop	r12
+	pop	r13
+	pop	r14
+	pop	r15
+
+	add	rsp, 16
+
+	iretq
