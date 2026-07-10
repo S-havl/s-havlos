f2535627c28f46a75a597de4700901dee8c328da
S-havl <pansillomcvalentincampostorres@gmail.com>
2026-07-09 19:21:47 -0500

    hybrid allman style to kprintf.c

--- libk/src/kprintf.c
+++ libk/src/kprintf.c
@@ -9,7 +9,8 @@
 static volatile uint16_t* const vga = (volatile uint16_t*)VGA_MEMORY;
 static size_t cursor = 0;
 
-void kprintf(const char* str) {
+void kprintf(const char* str)
+{
     for (size_t i = 0; str[i] != '\0'; i++) {
 
         if (str[i] == '\n') {
@@ -23,7 +24,8 @@ void kprintf(const char* str) {
     }
 }
 
-void clear_screen() {
+void clear_screen()
+{
     for (int i = 0; i < 80 * 25; i++) {
         vga[i] = 0x0720;
   }
