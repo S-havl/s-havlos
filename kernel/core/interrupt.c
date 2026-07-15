c78191c449aa87610840b6a6f830df18280d9447
S-havl <pansillomcvalentincampostorres@gmail.com>
2026-07-15 13:38:54 -0500

    refactor(kernel/core/interrupt.c): improving the interrupt dispatcher code

--- kernel/core/interrupt.c
+++ kernel/core/interrupt.c
@@ -1,34 +1,16 @@
 #include <stdint.h>
 
 typedef struct {
-    uint64_t r15;
-    uint64_t r14;
-    uint64_t r13;
-    uint64_t r12;
-    uint64_t r11;
-    uint64_t r10;
-    uint64_t r9;
-    uint64_t r8;
-
-    uint64_t rdi;
-    uint64_t rsi;
-    uint64_t rbp;
-    uint64_t rdx;
-    uint64_t rcx;
-    uint64_t rbx;
-    uint64_t rax;
+    uint64_t r15, r14, r13, r12, r11, r10, r9, r8;
+    uint64_t rdi, rsi, rbp, rdx, rcx, rbx, rax;
 
     uint64_t int_no;
     uint64_t err_code;
 
-    uint64_t rip;
-    uint64_t cs;
-    uint64_t rflags;
-    uint64_t rsp;
-    uint64_t ss;
-} __attribute__((packed)) InterruptFrame;
+    uint64_t rip, cs, rflags, rsp, ss;
+} __attribute__((packed)) interrupt_frame_t;
 
-void interrupt_dispatcher(InterruptFrame *frame)
+void interrupt_dispatcher(interrupt_frame_t *frame)
 {
     if (frame->int_no < 32) {
         // unused
