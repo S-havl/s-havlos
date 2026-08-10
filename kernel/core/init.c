6dc15366985068978b7fba1c19abd6342da1f3c7
S-havl <pansillomcvalentincampostorres@gmail.com>
2026-08-10 08:50:12 -0500

    refactor(init): removing software interrupts unused

--- kernel/core/init.c
+++ kernel/core/init.c
@@ -30,42 +30,6 @@ void kernel_init()
     interrupt_handlers_init();
     kprintf(COLOR_LIGHT_GREEN, COLOR_BLACK, "[ OK ] IDT HANDLERS initialized.\n");
 
-    // Remove all 'asm volatile' statements for production
-    /*
-    asm volatile ("int $0");
-    asm volatile ("int $1");
-    asm volatile ("int $2");
-    asm volatile ("int $3");
-    asm volatile ("int $4");
-    asm volatile ("int $5");
-    asm volatile ("int $6");
-    asm volatile ("int $7");
-    asm volatile ("int $8");
-    asm volatile ("int $9");
-    asm volatile ("int $10");
-    asm volatile ("int $11");
-    asm volatile ("int $12");
-    asm volatile ("int $13");
-    asm volatile ("int $14");
-    asm volatile ("int $15");
-    asm volatile ("int $16");
-    asm volatile ("int $17");
-    asm volatile ("int $18");
-    asm volatile ("int $19");
-    asm volatile ("int $20");
-    asm volatile ("int $21");
-    asm volatile ("int $22");
-    asm volatile ("int $23");
-    asm volatile ("int $24");
-    asm volatile ("int $25");
-    asm volatile ("int $26");
-    asm volatile ("int $27");
-    asm volatile ("int $28");
-    asm volatile ("int $29");
-    asm volatile ("int $30");
-    asm volatile ("int $31");
-    */
-
     // pic_init();
     // kprintf("[INFO] PIC initialized\n");
     
